652e43cf48d47280b99a7949e3eb782a2b115112
geezyy <xgweshyx@gmail.com>
2023-08-08 11:24:25 +0100

    commit

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+const filled =document.querySelector('.filled')
+
+function update () {
+    filled.style.width = `${((window.scrollY) / (document.body.scrollHeight - window.innerHeight) * 100)}%`
+    requestAnimationFrame(update);
+}
+
+update();
+
+
+function goToLink(link){
+    console.log(link.value);
+    // location.href = "https://www.linkedin.com/in/george-idode-246608285";
+    window.open(link.value)
+}
